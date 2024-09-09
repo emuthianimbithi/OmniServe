79fb4ff77b0e96f1ac98f03ac06c0d37e92392af
Emmanuel Mbithi <emmanuel@leta.ai>
2024-09-09 15:01:39 +0300

    configuration variable update

--- pkg/variables/variables.go
+++ pkg/variables/variables.go
@@ -15,3 +15,38 @@ var EntryPointTemplate = map[string]string{
 	"python":     "main.py",
 	"javascript": "main.js",
 }
+
+// Version of the application
+const Version = "0.1.0"
+
+const DefaultConfig = `# OmniServe CLI Configuration
+
+defaults:
+  language: go
+  license: MIT
+  version: 0.1.0
+  author: Your Name
+  git_init: true
+  dockerize: false
+
+paths:
+  templates: ~/.omniserve/templates
+
+languages:
+  go:
+    entry_point: main.go
+    build_command: go build
+  python:
+    entry_point: main.py
+    build_command: python -m compileall
+  javascript:
+    entry_point: index.js
+    build_command: npm run build
+  c:
+    entry_point: main.c
+    build_command: gcc -o main main.c
+
+cli:
+  verbose: false
+  color_output: true
+`
